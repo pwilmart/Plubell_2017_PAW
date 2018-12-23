4497a15c6c7f66555fe92807670b217417e8c1f1
pwilmart <wilmarth@ohsu.edu>
2018-12-23 12:35:42 -0800

    First notebook and README update

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Plubell_2017_PAW
-Data from Plubell et al., 2017 processed with PAW pipeline
+### Data from Plubell et al., 2017 processed with PAW pipeline
+
+A lot of work has happened since the 2017 MCP paper. In that first publication, IRS was done in Excel. It really is a simple enough idea to do that. It is labor intensive and possibly error-prone, though. I have a [data analysis pipeline](https://github.com/pwilmart/PAW_pipeline.git) originally developed for SEQUEST search results that is written in Python. It has nice protein inference and protein grouping steps. It has been updated to work with [Comet](http://comet-ms.sourceforge.net/) search results (at least up through 2016 versions).
+
+One thing I do not like about Proteome Discoverer, is the protein inference and how shared peptides are used in quantification. The PSM export files from PD have all of the same fields that [the PAW pipeline](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2816815/) needs, along with the reporter ion information. It is possible to take the confidently identified PSMs in the PD exports and run them through the later stages of the PAW pipeline. Support for TMT reporter ions was added.
+
+The PAW pipeline used MSConvert of the [ProteoWizard](http://proteowizard.sourceforge.net/) package to extract the MS2 scan information for Comet searches. Support to extract the reporter ion scan peak heights was added. Now the PAW pipeline can take TMT data exported from PD and produce protein-level quantitative reports, or data straight from RAW files in a full open source pipeline.
+
+The data from the original publication was depositied in [PRIDE](https://www.ebi.ac.uk/pride/archive/projects/PXD005953) and has been re-analyzed with Proteome Discoverer 2.2, PAW/Comet, and MaxQuant. Notebooks for analysis of these different workflows will be added eventually.
+
+One very important part of doing an IRS experiment that uses pooled internal standards, is making sure that those channels are correctly specified. There is nothing about the IRS procedure that has any knowledge of the correct channels to use for the internal standards except you! If you make a mistake in the standard channel designations, your data will get messed up. Like most computer use, there is no real way to protect you from yourself. The quality and accuracy of the record keeping is on you.
+
+That said, we can actually get the computers to help us double check our records of which channels were the pooled standard channels. The "auto_finder_PAW" notebook show you how to see which channels are the most similar in a TMT plex without specifying any sample information. The notebook reads the PAW results files, but the concepts would apply to other results files (PD or MaxQuant).
+
+I will add more content to this repository as time allows.
+
+December 23, 2018 - Phil W.
---
+++ auto_finder_PAW.r
@@ -0,0 +1,169 @@
+
+# library imports
+library(tidyverse)
+library(stringr)
+library(limma)
+library(psych)
+library(Matrix)
+
+# load the raw data
+# the table starts in Row 5
+# there are extra lines at the bottom - need to stop before the end
+data_import <- read_tsv("PAW_labeled_grouped_protein_summary_TMT_8.txt", 
+                        skip = 4, n_max = 4437, guess_max = 4437)
+
+# the "Filter" column flags contams and decoys
+data_all <- filter(data_import, is.na(Filter))
+
+# check number of rows before and after filtering
+cat(nrow(data_import), nrow(data_all))
+
+# make a data frame for each TMT-plex (and drop zeros)
+get_data <- function(df, substring) {
+    # extracts PAW TMT columns containing a substring
+    
+    # df: data frame of PAW results
+    # substring: a text substring that defines one TMT-plex
+    
+    # get the columns
+    df <- df %>%
+      select(starts_with("TotInt")) %>% 
+      select(contains(substring))
+    
+    # drop rows with zeros
+    df[apply(df, 1, function(row) all(row !=0 )), ] # return filtered frame
+}
+
+# get each experiment and set column names to something easy to parse
+A_raw <- get_data(data_all, "_A_")
+B_raw <- get_data(data_all, "_B_")
+C_raw <- get_data(data_all, "_C_")
+D_raw <- get_data(data_all, "_D_")
+
+# automatically find closest channel pairs
+
+SL_norm <- function(df, suffix, print_factors = TRUE) {
+    # Normalizes each channel's sum to the average grand total
+        # df: data frame of TMT data (one column for each channel)
+        # suffix: a text string to append to 1-to-n numbers for column names
+        # print_factors: logical to control printing
+    
+    # compute norm factors and scale columns
+    norm_facs <- mean(c(colSums(df))) / colSums(df)
+    df_sl  <- sweep(df, 2, norm_facs, FUN = "*")
+    
+    # simplify column names
+    colnames(df_sl) <- str_c(as.character(1:ncol(df)), suffix)
+    
+    # print the normalization factors for QC check
+    if (print_factors == TRUE) {
+        cat("\nNormalization Factors: ", suffix, "\n ")
+        cat(sprintf("%s - %0.3f\n", colnames(df), norm_facs))
+    }
+    
+    df_sl # return normalized data
+}
+
+diff <- function(df, x, y) {
+    # computes a standardized difference between two vectors
+        # df: data frame that contains x and y
+        # x: index of first column
+        # y: index of second column
+    
+    # compute the difference and set the column name
+    diff <- 100 * abs(df[x] - df[y]) / rowMeans(df[c(x, y)])
+    colnames(diff) <- sprintf("%d_%d", x, y)
+    
+    diff # return difference vector
+}
+
+iqr_grid <- function(df, print_grid = TRUE) {
+    # computes an NxN grid of interquartile ranges of diffs    
+         # df: a data frame of TMT data
+         # print_grid: logical to control printing
+    
+    # make an empty matrix ncol-by-ncol
+    iqrs  <- matrix(0.0, ncol(df), ncol(df))
+    
+    # populate an upper diagonal "diff" matrix
+    for (i in 1:ncol(df)) {
+        for (j in (i+1):ncol(df)) {
+            if (j > ncol(df)) {
+                break
+            }
+            iqrs[i, j] <- IQR(diff(df, i, j)[[1]])
+        }
+    }
+    
+    # print the grid
+    if( print_grid == TRUE) {
+        cat("\nInterquartile range grid:\n")
+        print(round(iqrs, 2))
+    }
+    
+    iqrs # return the iqr grid
+}
+
+find_best_pair <- function(df, nmax, suffix, title) {
+    # finds channel pairs having the smallest standardized differences    
+        # df: data frame with TMT channels
+        # nmax: how many channels/pairs to check
+        # suffix: suffix to add to channel labels
+        # title: title string to use in plots
+    
+    # first do SL normalization
+    df_sl <- SL_norm(df, suffix)
+    
+    # compute the ird grid
+    iqrs <- iqr_grid(df_sl)
+    
+    # find the nmax number of smallest IQR values
+    # from https://stackoverflow.com/questions/38664241/ranking-and-counting-matrix-elements-in-r
+    iqrs_long <- summary(Matrix(iqrs, sparse = TRUE))
+    iqrs_ordered <- iqrs_long[order(-iqrs_long[, 3], decreasing = TRUE),]
+    candidates <- iqrs_ordered[1, 1] # starting vector of channels
+    for (row in 1:nrow(iqrs_long)){
+        candidates <- c(candidates, iqrs_ordered[row, 1])
+        candidates <- c(candidates, iqrs_ordered[row, 2])
+    }
+    
+    # get the set of indexes and print the best pairs
+    for (pair in 1:nmax) {
+        top_n <- sort(unique(candidates)[1:nmax])
+        i <- iqrs_ordered[pair, 1]
+        j <- iqrs_ordered[pair, 2]
+        cat(sprintf("\nPair #%d: (%d, %d) => %0.2f", pair, i, j, iqrs[i, j]))
+    }
+    
+    # make the box plots for the top-n pairs
+    distributions <- list()
+    for (row in 1:nmax){
+        distributions[[row]] <- diff(df_sl, iqrs_ordered[row, 1], iqrs_ordered[row, 2])
+    }
+    boxplot(do.call(cbind, distributions), main = title)
+    
+    # multi-panel scatter plots for the top-n channels
+    cat(pairs.panels(log10(df_sl[top_n]), lm = TRUE, main = title))
+}
+
+# nmax is how many top pairs to consider (3 or 4 are recommended values)
+top_n <- 4
+
+# find the standards in experiment A
+# this is now a one-line function call
+find_best_pair(A_raw, top_n, "_A", "Exp A")
+
+# experiment B
+find_best_pair(B_raw, top_n, "_B", "Exp B")
+
+# experiment C
+find_best_pair(C_raw, top_n, "_C", "Exp C")
+
+# and experiment D
+find_best_pair(D_raw, top_n, "_D", "Exp D")
+
+# check the clustering
+A_sl <- SL_norm(A_raw, "_A")
+plotMDS(log10(A_sl), main = "Experiment A")
+
+pairs.panels(log10(A_sl), main = "Exp. A - all 10")
